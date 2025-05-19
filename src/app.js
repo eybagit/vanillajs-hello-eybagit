b8b012bb021ae0f85922e5323aaa5a7492415192
Elkin Botero <147097939+eybagit@users.noreply.github.com>
2025-05-19 02:04:08 +0000

    GeneradorExcusasJs

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'Bobby'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function getRandomNumber(a, b){
+  return Math.floor(Math.random()*(b-a)+a)
+}
+
+function getRandom(anyArray){
+  let max = anyArray.length -1;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
+
+window.onload = function () {
+  const excuseElement = document.querySelector('#excuse');
   //write your code here
   console.log("Hello Rigo from the console!");
+    excuseElement.innerHTML = `<h1>${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)}</h1>`;
 };
+
